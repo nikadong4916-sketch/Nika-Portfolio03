ccf3f2c25e0d36fb4c88d9c7082dbc3c91e7ed6b
nikadong4916-sketch <nikadong4916@gmail.com>
2026-05-04 07:21:47 +0800

    Update ProjectDetail.tsx

--- ProjectDetail.tsx
+++ ProjectDetail.tsx
@@ -1,6 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import { motion } from "motion/react";
-import { projects } from "../data";
+import { projects } from "./data";
 import { ArrowLeft, ExternalLink, Calendar, Tag } from "lucide-react";
 
 export default function ProjectDetail() {
